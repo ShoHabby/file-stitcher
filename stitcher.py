5731aec11978a451794fc957648821711c384848
Christophe Savard <christophe_savard@hotmail.ca>
2024-11-24 15:05:18 -0500

    Add checking that folders to merge contain valid files

--- stitcher.py
+++ stitcher.py
@@ -1,17 +1,28 @@
 ﻿import os
 import multiprocessing
 import subprocess
+import sys
+from os import DirEntry
 from typing import List
 
+
 def main() -> None:
+    args: List[str] = sys.argv[1:]  # Don't want the script path
+    vertical_stitch(args)
+
+def vertical_stitch(args: List[str]) -> None:
+    print("Vertical stitching of all subfolders...")
     cwd: str = os.getcwd()
-    subdirs: List[str] = [f.name for f in os.scandir(cwd) if f.is_dir()]
+    subdirs: List[str] = [entry.name for entry in os.scandir(cwd) if is_valid_dir(entry)]
     with multiprocessing.Pool() as pool:
         pool.map(stitch_subdir, subdirs)
 
+def is_valid_dir(entry: DirEntry[str]) -> bool:
+    return entry.is_dir() and any(f.is_file() and f.name.endswith(".png") for f in os.scandir(entry.path))
+
 def stitch_subdir(subdir: str) -> None:
-    args: List[str] = ["magick", "convert", "-append", f"{subdir}/*.png", f"{subdir}.png"]
-    subprocess.call(args)
+    command: List[str] = ["magick", "convert", "-append", f"{subdir}/*.png", f"{subdir}.png"]
+    subprocess.call(command)
     print(f"Folder {os.path.join(os.getcwd(), subdir)} stitched to file {subdir}.png")
 
 if __name__ == "__main__":
